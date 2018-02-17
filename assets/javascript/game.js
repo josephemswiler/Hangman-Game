346a8c2da46026c4e94c2058693deab3a4d19f5c
josephemswiler <josephemswiler@gmail.com>
2018-02-17 02:52:41 -0600

    draft final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,161 +5,311 @@
     var correctGuessedAlpha = [];
     var guessCount = 6;
     var winCount = 0;
-    var movieChoices = ["Jurassic World", "Star Wars", "Indiana Jones", "Wonder Woman", "Get Out", "Logan", "The Big Sick"];
-    var selectedTitle = [];
-    var gameOn = false;
+    var charChoices = ["Eleven", "Dustin Henderson", "Mike Wheeler", "Lucas Sinclair", "Will Byers", "Chief Hopper", "Joyce Byers", "Nancy Wheeler", "Jonathan Byers", "Steve Herrington", "Barb!", "Demogorgon"];
+    var selectedChar = [];
     var charCodes = [];
-    var movieTitle = movieChoices[Math.floor(Math.random() * movieChoices.length)];
-    movieTitle = movieTitle.toLowerCase();
     var correctGuess = false;
     var currentState = [];
+    var gameOver = true;
+    var charName = charChoices[Math.floor(Math.random() * charChoices.length)];
+    var gameSet = true;
 
+    charName = charName.toLowerCase();
 
+    var audio = new Audio("assets/audio/stranger-things-music.mp3");
+    audio.addEventListener('canplaythrough', function () {
+
+        audio.play();
+
+    }, false);
+
+    var nameLetters;
 
     function createAlphabet() {
         for (var i = 0; i < 26; i++) {
+
             var newAnchor = document.createElement("a");
             newAnchor.innerHTML = (i + 10).toString(36).toUpperCase();
             newAnchor.setAttribute("class", ((i + 10).toString(36)));
             document.querySelector(".letters-to-guess").appendChild(newAnchor);
             alpha.push((i + 10).toString(36));
+
         }
+
     }
 
     document.body.onload = createAlphabet;
 
-    var keyFunction = function (event) {
+    function startGame() {
 
-        function startGame() {
+        document.querySelector(".reset-game").style.display = 'none';
 
-            for (var j = 0; j < movieTitle.length; j++) {
+        document.querySelector(".main").style.display = 'none';
 
-                charCodes.push(movieTitle.charCodeAt(j));
+        for (var j = 0; j < charName.length; j++) {
 
-                var titleLetters = document.createElement("span");
-                //if space, show, else hide character
-                if (movieTitle.charCodeAt(j) === 32) {
-                    titleLetters.innerHTML = " ";
-                } else {
-                    titleLetters.innerHTML = "-";
-                    selectedTitle.push(movieTitle[j]);
-                }
-                //show non-alpha characters
-                if (movieTitle.charCodeAt(j) < 65) {
-                    titleLetters.innerHTML = movieTitle.charAt(j);
-                }
-                //number class
-                //titleLetters.setAttribute("class", "letter-" + j);
-                //letter class
-                titleLetters.setAttribute("class", "letter-" + movieTitle.charAt(j));
-                document.querySelector(".movie-title").appendChild(titleLetters);
+            charCodes.push(charName.charCodeAt(j));
 
-                //console.log(selectedTitle);
-                gameOn = true;
+            nameLetters = document.createElement("span");
+
+            //if space, show, else hide character
+            if (charName.charCodeAt(j) === 32) {
+
+                nameLetters.innerHTML = " ";
+
+            } else {
+
+                nameLetters.innerHTML = "-";
+                selectedChar.push(charName[j]);
+
             }
 
-        } // /startGame
+            //show non-alpha characters
+            if (charName.charCodeAt(j) < 65) {
 
-        //run startGame
-        if (guessCount > 0 && gameOn === false) {
-            document.querySelector(".main").style.display = 'none';
-            startGame();
+                nameLetters.innerHTML = charName.charAt(j);
+
+            }
+
+            nameLetters.setAttribute("class", "letter-" + charName.charAt(j));
+            document.querySelector(".char-name").appendChild(nameLetters);
+            gameOver = false;
         }
 
-        //check for correct guess of letter
-        for (var k = 0; k < movieTitle.length; k++) {
+    } // /startGame
 
-            if (event.key === movieTitle.charAt(k)) {
-                //var classString = ".letter-" + movieTitle.indexOf(event.key);
-                //document.querySelector(classString).innerHTML = movieTitle.charAt(k);
+    //check for correct guess of letter
+    function checkLetter(letterToCheck) {
 
-                //var classString = ".letter-" + event.key;
-                //document.querySelector(classString).innerHTML = event.key;
+        for (var k = 0; k < charName.length; k++) {
 
-                var correctClass = document.body.querySelectorAll(".letter-" + event.key);
+            if (letterToCheck === charName.charAt(k) && gameOver === false) {
+
+                var correctClass = document.body.querySelectorAll(".letter-" + letterToCheck);
 
                 for (var l = 0; l < correctClass.length; l++) {
-                    correctClass[l].innerHTML = event.key;
-                    //console.log(correctClass[l]);
+
+                    correctClass[l].innerHTML = letterToCheck;
+
                 }
+
                 correctGuess = true;
-                correctGuessedAlpha.push(event.key);
-                //console.log(correctGuessedAlpha);
-                
+
+                correctGuessedAlpha.push(letterToCheck);
+
                 //check if game is over
-                /*
-                for (var m = 0; m < selectedTitle.length; m++) {
-                    var isWin = correctClass[m].innerHTML
-                    if (isWin.indexOf("-") !== -1 )
-                }
-                */
+                var childTitles = document.querySelector(".char-name").children;
 
-                var childTitles = document.querySelector(".movie-title").children;
                 for (var m = 0; m < childTitles.length; m++) {
-                    //correctClass[l].innerHTML = event.key;
-                    
-                    currentState.splice(m,1,childTitles[m].innerHTML);
+
+                    currentState.splice(m, 1, childTitles[m].innerHTML);
+
                 }
 
-                if (currentState.indexOf("-") === -1) {
-                    console.log('win bruh!');
+                if ((currentState.indexOf("-")) === -1 && (gameOver === false)) {
+
                     winCount++
                     document.querySelector(".win-count").innerHTML = winCount;
-                    document.querySelector(".already-guessed").innerHTML = "YOU WIN! Click here to play again!";
-                    document.querySelector(".already-guessed").style.visibility = 'visible';
+                    document.querySelector(".instructions").style.display = 'none';
+                    document.querySelector(".already-guessed").style.display = 'none';
+                    document.querySelector(".already-guessed").style.visibility = 'hidden';
+                    document.querySelector(".result").innerHTML = "YOU WIN!";
+                    document.querySelector(".result").style.visibility = 'visible';
+                    document.querySelector(".result").style.display = 'block';
+                    document.querySelector(".reset-game").style.display = 'block';
+                    document.querySelector(".reset-game").innerHTML = "Play Again";
+                    document.querySelector(".char-name").innerHTML = charName;
+                    gameOver = true;
+                    gameSet = false;
+
                 }
 
             }
 
-        }
+        } // /for loop
 
-        //remove letters already guessed from alphabet array
-        if (alpha.indexOf(event.key) !== -1) {
-            document.querySelector("." + event.key).innerHTML = "-";
-        }
-        //check if letter has already been guessed
-        if ((alpha.indexOf(event.key) === -1) && (guessedAlpha.indexOf(event.key) === -1)) {
+    } // /checkLetter
 
-        } else if ((alpha.indexOf(event.key) === -1)) {
-            document.querySelector(".already-guessed").innerHTML = event.key.toUpperCase() + " already guessed!";
-            document.querySelector(".already-guessed").style.visibility = 'visible';
+    //check if letter has already been guessed
+    function isLetterGuessed(letterGuessed) {
 
-            setTimeout(function () {
-                document.querySelector(".already-guessed").style.visibility = 'hidden';
-            }, 1000);
+        if (gameOver === false) {
 
-        } else {
-            var guess = alpha.splice(alpha.indexOf(event.key), 1);
-            guess = guess.join();
-            guessedAlpha.push(guess);
+            if ((alpha.indexOf(letterGuessed) === -1) && (guessedAlpha.indexOf(letterGuessed) === -1)) {} else if ((alpha.indexOf(letterGuessed) === -1)) {
 
-            if (guessCount < 1) {
-                document.querySelector(".main").innerHTML = "GAME OVER";
-                document.querySelector(".main").style.display = 'block';
-                document.querySelector(".movie-title").style.display = 'none';
-                //alert to play again?
-                //document.querySelector(".already-guessed").innerHTML = "Play Again"
-                //show answer, might want to make two divs for messages and game play
+                document.querySelector(".instructions").style.display = 'none';
+                document.querySelector(".result").style.display = 'none';
+                document.querySelector(".result").style.visibility = 'hidden';
+                document.querySelector(".already-guessed").innerHTML = letterGuessed.toUpperCase() + " already guessed!";
+                document.querySelector(".already-guessed").style.display = 'block';
+                document.querySelector(".already-guessed").style.visibility = 'visible';
 
-                //reset game
-            } else if (correctGuess === false) {
+            } else {
+
+                var guess = alpha.splice(alpha.indexOf(letterGuessed), 1);
+                guess = guess.join();
+                guessedAlpha.push(guess);
+
+            }
+
+
+            if (correctGuess === false) {
+
                 guessCount--
                 document.querySelector(".guess-counter").innerHTML = guessCount;
+
+                if (guessCount < 1) {
+
+                    document.querySelector(".instructions").style.display = 'none';
+                    document.querySelector(".already-guessed").style.display = 'none';
+                    document.querySelector(".already-guessed").style.visibility = 'hidden';
+                    document.querySelector(".result").innerHTML = "GAME OVER";
+                    document.querySelector(".result").style.display = 'block';
+                    document.querySelector(".result").style.visibility = 'visible';
+                    document.querySelector(".reset-game").style.display = 'block';
+                    document.querySelector(".reset-game").innerHTML = "Play Again";
+                    document.querySelector(".char-name").innerHTML = charName;
+                    gameOver = true;
+                    gameSet = false;
+                }
+
             } else {
+
                 correctGuess = false;
+
             }
+
+            let timingFunction = setTimeout(function () {
+
+                document.querySelector(".already-guessed").style.display = 'none';
+                document.querySelector(".already-guessed").style.visibility = 'hidden';
+
+            }, 2000);
+
+
         }
-        //check if game is over
 
+    } // /isLetterGuessed
 
+    //remove letters already guessed from alphabet array
+    function removeLetters(lettersToRemove) {
+
+        if (alpha.indexOf(lettersToRemove) === -1) {
+
+            document.querySelector("." + lettersToRemove).innerHTML = "-";
+
+        }
+
+    };
+
+    var keyFunction = function (event) {
+
+        //run startGame
+        if (gameOver === true && gameSet === true) {
+
+            startGame();
+
+        };
+
+        if (gameOver === false && gameSet === true && event.key.charCodeAt(0) >= 97 && event.key.charCodeAt(0) <= 122) {
+
+            checkLetter(event.key);
+
+            isLetterGuessed(event.key);
+
+            removeLetters(event.key);
+
+        }
 
     } // /keyFunction
 
-
-
     document.onkeydown = keyFunction;
 
+    var clickFunction = function (event) {
 
+        //run startGame
+        if (gameOver === true && gameSet === true) {
+
+            startGame();
+
+        }
+
+        if (gameOver === false && gameSet === true && event.target.classList[0] && event.target.classList[0].length === 1 && event.target.classList[0].charCodeAt(0) >= 97 && event.target.classList[0].charCodeAt(0) <= 122) {
+
+            checkLetter(event.target.classList[0].toLowerCase());
+
+            isLetterGuessed(event.target.classList[0].toLowerCase());
+
+            removeLetters(event.target.classList[0].toLowerCase());
+
+        }
+
+    } // /clickFunction
+
+    document.onclick = clickFunction;
+
+    function resetVar() {
+
+        alpha = [];
+        guessedAlpha = [];
+        correctGuessedAlpha = [];
+        guessCount = 7;
+        selectedChar = [];
+        charCodes = [];
+        currentState = [];
+        correctGuess = false;
+        gameSet = true;
+
+    };
+
+    function hideRemove() {
+
+        document.querySelector(".result").style.display = 'none';
+        document.querySelector(".result").style.visibility = 'hidden';
+        document.querySelector(".already-guessed").style.display = 'none';
+        document.querySelector(".already-guessed").style.visibility = 'hidden';
+        document.querySelector(".reset-game").style.display = 'none';
+        var lettersToClear = document.querySelector(".letters-to-guess");
+
+        while (lettersToClear.firstChild) {
+
+            lettersToClear.removeChild(lettersToClear.firstChild);
+
+        };
+
+        var nameToClear = document.querySelector(".char-name");
+
+        while (nameToClear.firstChild) {
+
+            nameToClear.removeChild(nameToClear.firstChild);
+
+        };
+
+    };
+
+    function showAdd() {
+
+        document.querySelector(".instructions").style.display = 'block';
+        document.querySelector(".main").style.display = 'block';
+        createAlphabet();
+        charName = charChoices[Math.floor(Math.random() * charChoices.length)];
+        charName = charName.toLowerCase();
+
+    };
+
+
+    function resetGame() {
+
+        //reset
+        resetVar();
+        //hide
+        hideRemove();
+        //show
+        showAdd();
+
+    };
+
+    document.querySelector('.reset-game').onclick = resetGame;
 
     //blinking text
     var textHidden = false;
@@ -168,10 +318,15 @@
 
     function textBlink() {
         if (textHidden) {
+
             document.querySelector(".main").style.visibility = 'visible';
+
         } else {
+
             document.querySelector(".main").style.visibility = 'hidden';
+
         }
+
         textHidden = !textHidden;
     } // /blinking text
 
